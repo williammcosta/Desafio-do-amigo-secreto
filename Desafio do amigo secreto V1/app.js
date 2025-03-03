15d382a8e0c8dc9808cbff02639e8feba7febcb5
williammcosta <wmc1617@gmail.com>
2025-03-03 10:55:17 -0300

    Iniciou o projeto
    Primeiros passos foram iniciado

--- Desafio do amigo secreto V1/app.js
+++ Desafio do amigo secreto V1/app.js
@@ -1,1 +1,38 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas 
+// habilidades em lógica de programação. Aqui você deverá 
+// desenvolver a lógica para resolver o problema.
+
+//Laço de repetição
+
+
+//Variavel que vai recebe os dados para faze o sorteio.
+/*function adicionarAmigo(){
+    let amigo =document.querySelector('input').value;
+    
+}*/
+
+//let campooParaSorteio = prompt  ("Teste");
+
+let participantes = [];
+let nome = '';
+function adicionarAmigo() 
+{
+    nome = document.getElementById("amigo").value;
+    if (nome != '') 
+    {
+    participantes.push(nome);
+    let lista = document.getElementById("listaAmigos");
+    let item = document.createElement("li");
+    item.textContent = nome;
+    lista.appendChild(li);
+    //atualizarLista();
+    nome = '';
+    }
+    
+    console.log(`${nome} foi add! `); 
+}
+/*function atualizarLista()
+{
+  let lista = document.getElementById("");
+
+}*/
